3326db3117ed835f04e1035945862dc7a108ffdb
boryeira <jmanuel.jorquera@gmail.com>
2019-11-13 02:37:20 -0300

    flow production

--- app/Http/Controllers/Flow/FlowController.php
+++ app/Http/Controllers/Flow/FlowController.php
@@ -17,14 +17,14 @@ class FlowController extends Controller
     public function __construct()
     {
         
-      // $flow = Flow::make('production', [
-      //   'apiKey'    => '7B19A4CF-F041-40C4-9488-4180L75A6AAA',
-      //   'secret'    => '8a8c824cd4550b1ee4d581a1d3404d9d640638b0',
-      // ]);
-      $this->flow = Flow::make('sandbox', [
-              'apiKey'    => '367F3C6A-DEB8-46F7-89E5-32CLED2236B9',
-              'secret'    => '65d9f9656b478aaa7be72267bc33f40747f47c94',
-          ]);
+      $flow = Flow::make('production', [
+        'apiKey'    => '7B19A4CF-F041-40C4-9488-4180L75A6AAA',
+        'secret'    => '8a8c824cd4550b1ee4d581a1d3404d9d640638b0',
+      ]);
+      // $this->flow = Flow::make('sandbox', [
+      //         'apiKey'    => '367F3C6A-DEB8-46F7-89E5-32CLED2236B9',
+      //         'secret'    => '65d9f9656b478aaa7be72267bc33f40747f47c94',
+      //     ]);
     
     }
 
